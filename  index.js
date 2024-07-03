836b9aa5766fe544fea8e21749b84fa9b09c2c88
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2024-07-03 06:23:38 +0000

    Ready to deploy

---  index.js
+++  index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('dotenv').config()
 const DB = require('./Config/database');
 const logger = require('./Config/logger');
 
 const app = express();
 const port = 3000;
 
+//middlewares
+app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use('/',require('./Router'));
 app.listen(port, () => {
